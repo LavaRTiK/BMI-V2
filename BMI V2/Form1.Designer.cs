e03aea1d7a764ba381f7ecf3b926eeef3fcf2bd7
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2024-04-08 22:57:29 +0300

    Add project files.

---
+++ BMI V2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace BMI_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(359, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 0;
+            textBox1.Validating += textBox1_Validating;
+            textBox1.Validated += textBox1_Validated;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(359, 151);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 1;
+            textBox2.Validating += textBox2_Validating;
+            textBox2.Validated += textBox2_Validated;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(270, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Зріст";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(270, 157);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Вага";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(359, 242);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 25);
+            label3.TabIndex = 4;
+            label3.Text = "IMT:";
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ BMI V2/Form1.cs
@@ -0,0 +1,78 @@
+﻿namespace BMI_V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string errorMsg;
+            if (!ValidationNumbers(textBox1.Text, out errorMsg))
+            {
+                e.Cancel = true;
+                textBox1.Select(0, textBox1.Text.Length);
+
+                this.errorProvider1.SetError(textBox1, errorMsg);
+            }
+        }
+        public bool ValidationNumbers(string text, out string errorMassege)
+        {
+            if (text.Length == 0)
+            {
+                errorMassege = "Введіть якесь число";
+                return false;
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!char.IsNumber(text[i]))
+                {
+                    errorMassege = "Невірний формат";
+                    return false;
+                }
+            }
+            errorMassege = "";
+            return true;
+        }
+
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(textBox1, "");
+            checkRule();
+        }
+
+        private void textBox2_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string errorMessage;
+            if (!ValidationNumbers(textBox2.Text, out errorMessage))
+            {
+                e.Cancel = true;
+                textBox1.Select(0, textBox1.Text.Length);
+
+                this.errorProvider1.SetError(textBox2, errorMessage);
+            }
+        }
+
+        private void textBox2_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(textBox2, "");
+            checkRule();
+        }
+
+        private void checkRule()
+        {
+            if (textBox1.Text.Length != 0 && textBox2.Text.Length != 0)
+            {
+                double result = double.Parse(textBox2.Text)/((double.Parse(textBox1.Text)/100)*(double.Parse(textBox1.Text) / 100));
+                label3.Text ="ІМТ:" + Math.Round(result, 2).ToString();
+            }
+        }
+    }
+}
